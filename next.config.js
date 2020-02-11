f83138b350dba03815a240a83385bc88662516eb
Ben Hadfield <ben.hadfield@deliveroo.co.uk>
2020-02-11 00:23:31 +0000

    add repo to package.json

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 const withMDX = require('@next/mdx')()
 
-module.exports = withMDX()
+module.exports = withMDX({
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) {
+      config.node = {
+        fs: 'empty',
+      }
+    }
+
+    return config
+  }
+})
