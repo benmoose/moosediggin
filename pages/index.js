3f5b34e89755920f0a4fd3dd4a2a569382105409
Ben Hadfield <ben.hadfield@deliveroo.co.uk>
2020-02-11 12:51:49 +0000

    throw error if pages are missing metadata

--- pages/index.js
+++ pages/index.js
@@ -8,16 +8,28 @@ const Home = ({ pages }) => {
 
 Home.getInitialProps = function () {
   const pages = (context => {
-    const keys = context.keys()
-    const pages = keys.map(key => {
-      const { title, date, imageURL, snippetText } = context(key)
-      const href = key.slice(0, -4)
-      return { title, date, imageURL, snippetText, href }
-    })
-    return pages
+    return context.keys().map(key => getPage(context, key))
   })(require.context("./", false, /\.mdx$/))
 
   return { pages: pages.sort((a, b) => a.date > b.date ? -1 : 1) }
+}
+
+function getPage (context, key) {
+  const { title, date, imageURL, snippetText } = context(key)
+  if (!title) {
+    throw Error(`${key} is missing required metadata 'title'`)
+  }
+  if (!date) {
+    throw Error(`${key} is missing required metadata 'date'`)
+  }
+  if (!imageURL) {
+    throw Error(`${key} is missing required metadata 'imageURL'`)
+  }
+  if (!snippetText) {
+    throw Error(`${key} is missing required metadata 'snippetText'`)
+  }
+  const href = key.slice(0, -4)
+  return { title, date, imageURL, snippetText, href }
 }
 
 export default Home
